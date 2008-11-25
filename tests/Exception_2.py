455825268be55a4b7b6001159b89b1f4716eb657
ktlim <ktlim@git.lsstcorp.org>
2008-11-25 21:55:14 +0000

    Use proper unittest/lsst.utils.tests structure for Python test.

--- tests/Exception_2.py
+++ tests/Exception_2.py
@@ -1,21 +1,64 @@
+import unittest
+import lsst.utils.tests as tests
+
 import lsst.pex.exceptions
 import failer
-import sys
 
-x = failer.Failer()
-try:
-    x.fail()
-except lsst.pex.exceptions.Exception, e:
-    print "Caught Exception"
-    print e.what()
-except failer.MyException, e:
-    print "Caught MyException"
-    print e.what()
-    print issubclass(sys.exc_type, lsst.pex.exceptions.Exception)
-    print isinstance(sys.exc_value, failer.MyException)
-    print isinstance(sys.exc_value, lsst.pex.exceptions.Exception)
-except:
-    print issubclass(sys.exc_type, lsst.pex.exceptions.Exception)
-    print isinstance(sys.exc_value, failer.MyException)
-    print isinstance(sys.exc_value, lsst.pex.exceptions.Exception)
-    print sys.exc_info()
+class ExceptionTestCase(unittest.TestCase):
+    """A test case for C++/Python LsstCppExceptions."""
+    def setUp(self):
+        """Create a wrapped C++ instance that throws an exception."""
+        self.x = failer.Failer()
+
+    def tearDown(self):
+        del self.x
+
+    def testHierarchy(self):
+        """Check that the LsstCppException class hierarchy is correct and that
+        the C++ exception is a subclass of lsst.pex.exceptions.Exception."""
+        issubclass(lsst.pex.exceptions.LsstException, Exception)
+        issubclass(lsst.pex.exceptions.LsstCppException,
+                lsst.pex.exceptions.LsstException)
+        issubclass(failer.MyException, lsst.pex.exceptions.Exception)
+
+    def testThrow(self):
+        """Check that the exception thrown is a Python Exception, an
+        LsstException, and an LsstCppException."""
+        self.assertRaises(Exception, self.x.fail)
+        self.assertRaises(lsst.pex.exceptions.LsstException, self.x.fail)
+        self.assertRaises(lsst.pex.exceptions.LsstCppException, self.x.fail)
+
+    def testValue(self):
+        """Check that the exception value is a proper SWIGged C++ exception
+        with appropriate message."""
+        try:
+            self.x.fail()
+        except Exception, e:
+            self.assert_(e is not None)
+            self.assertEqual(len(e.args), 1)
+            self.assert_(isinstance(e, Exception))
+            self.assert_(isinstance(e, lsst.pex.exceptions.LsstException))
+            self.assert_(isinstance(e, lsst.pex.exceptions.LsstCppException))
+            self.assert_(isinstance(e.args[0], lsst.pex.exceptions.Exception))
+            self.assert_(isinstance(e.args[0], failer.MyException))
+            self.assertEqual(e.args[0].what(),
+                    "0: failer::MyException thrown at tests/Failer.cc:7 " +
+                    "in void failer::Failer::fail()\n0: Message: message\n")
+            self.assertEqual(str(e),
+                    "0: failer::MyException thrown at tests/Failer.cc:7 " +
+                    "in void failer::Failer::fail()\n0: Message: message\n")
+
+def suite():
+    """Returns a suite containing all the test cases in this module."""
+    tests.init()
+
+    suites = []
+    suites += unittest.makeSuite(ExceptionTestCase)
+    return unittest.TestSuite(suites)
+
+def run(exit=False):
+    """Run the tests"""
+    tests.run(suite(), exit)
+
+if __name__ == '__main__':
+    run(True)
