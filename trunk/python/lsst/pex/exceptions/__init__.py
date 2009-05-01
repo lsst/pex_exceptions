b45f49b38b30419a0e7008d14b207154d224a9a0
ktlim <ktlim@git.lsstcorp.org>
2009-05-01 19:10:13 +0000

    Remove inadvertent copy.

--- trunk/python/lsst/pex/exceptions/__init__.py
+++
@@ -1,1 +0,0 @@
-from exceptionsLib import *
--- trunk/tests/Failer.cc
+++
@@ -1,8 +0,0 @@
-#include "Failer.h"
-
-failer::Failer::Failer(void) {
-}
-
-void failer::Failer::fail(void) {
-    throw LSST_EXCEPT(failer::MyException, "message");
-};
