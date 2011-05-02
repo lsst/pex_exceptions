8f24a72ff6291cce16b3083ddcca8e7d8feb9052
jbosch <jbosch@git.lsstcorp.org>
2011-05-02 08:11:20 +0000

    bp metapackage - added pex_policy, bpdox label and ignore aliases added to doxygen configuration, doxygen configurations unified

--- python/lsst/pex/exceptions/__init__.py
+++ python/lsst/pex/exceptions/__init__.py
@@ -7,13 +7,7 @@ class Exception(object):
     def __str__(self):
         return str(self.args[0])
 
-    def getType(self):
-        return self.args[0].getType()
-
-    def getTraceback(self):
-        return self.args[0].getTraceback()
-
-    def what(self):
-        return self.args[0].what()
+    def __getattr__(self, name):
+        return getattr(self.args[0], name)
 
 lsst.bputils.rescope(_exceptions, globals(), ignore=("Exception",))
